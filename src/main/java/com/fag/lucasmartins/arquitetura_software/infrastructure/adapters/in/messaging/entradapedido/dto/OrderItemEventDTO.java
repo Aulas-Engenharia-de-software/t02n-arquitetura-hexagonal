1cd9701c655bc5bf934406158090dadfc5bb8f4b
Eduardo Bertolin <eduardo_bertolin@hotmail.com>
2026-04-28 21:20:52 -0300

    adicionar DTOs do evento de pedido SQS

---
+++ src/main/java/com/fag/lucasmartins/arquitetura_software/infrastructure/adapters/in/messaging/entradapedido/dto/OrderItemEventDTO.java
@@ -0,0 +1,24 @@
+package com.fag.lucasmartins.arquitetura_software.infrastructure.adapters.in.messaging.entradapedido.dto;
+
+public class OrderItemEventDTO {
+
+    private Integer sku;
+
+    private Integer amount;
+
+    public Integer getSku() {
+        return sku;
+    }
+
+    public void setSku(Integer sku) {
+        this.sku = sku;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+}
